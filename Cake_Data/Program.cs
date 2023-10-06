6f8fc708480d11a0783aba28ed755954e7d9b3fd
Quynh Nhung <55863320+nhungnqwork@users.noreply.github.com>
2023-10-06 14:42:56 +0700

    Update Program.cs

--- Cake_Data/Program.cs
+++ Cake_Data/Program.cs
@@ -8,20 +8,24 @@
 
 #region Cake Data
 using System.Xml.Linq;
-
-ushort muffins = 1 | (1 << 5);          //id = 1, price = $1 
-                    //$50               //0_00001 | 1_00000
-                                        // 0b0001_00001
-ushort chocco_muffins = 2 | (2 << 5);   //id = 2, price = $2
-                   //$100               //0_00010 | 0_00010
-                                        //  0b0100_0010
-ushort de_muffins_theme = 3 | (3 << 5); //id = 3, price: $3 //$150
-ushort eggless_cake = 4 | (50 << 5);     //id = 4, price: $4 //$2500
-ushort diabetie_cake = 5 | (50 << 5);     //id = 5, price: $4 //$2500
-ushort black_forest_cake = 6 | (34 << 5); // id = 6, //$1700
-ushort red_velvet_cheese_cake = 7  | (60 << 5); // id = 7, //$3000
-ushort caramel_cake = 8  | (40 << 5); // id = 8, //$2000
-ushort choco_mar_cake = 9 | (36 << 5); //id = 9, //$1800
+ushort famous_house_signatureredvelvet = 12 | (50 << 5); //id = 12, $2500
+ushort muffins = 1 | (1 << 5);                              //id = 1, price = $1 
+                    //$50                               //0_00001 | 1_00000
+                                                        // 0b0001_00001
+ushort chocco_muffins = 2 | (2 << 5);                   //id = 2, price = $2
+                   //$100                               //0_00010 | 0_00010
+                                                        //  0b0100_0010
+ushort de_muffins_theme = 3 | (3 << 5);                 //id = 3, price: $3 //$150
+ushort eggless_cake = 4 | (50 << 5);                    //id = 4, price: $4 //$2500
+ushort diabetie_cake = 5 | (50 << 5);                   //id = 5, price: $4 //$2500
+ushort black_forest_cake = 6 | (34 << 5);               // id = 6, //$1700
+ushort red_velvet_cheese_cake = 7  | (60 << 5);         // id = 7, //$3000
+ushort caramel_cake = 8  | (40 << 5);                   // id = 8, //$2000
+ushort choco_mar_cake = 9 | (36 << 5);                  //id = 9, //$1800
+ushort strawberry_vanilla = 10 | (36 << 5);             // id = 10, //$1800
+ushort ginger_lemon = 11 | (40 << 5);                   // id = 11, //$2000
+ushort coffee_nut = 12 | (44 << 5);                     // id = 12, //$2000
+ushort vanilla_choco = 13 | (34 << 5);                   // id = 13, //$1700
 #endregion
 
 
@@ -33,17 +37,29 @@ void DisplayCake(string name, ushort cake)
     Console.WriteLine($"---------------------------");
     Console.WriteLine($"Binary Layout: {Convert.ToString(cake,2).PadLeft(11,'0')}");
     Console.WriteLine($"Temporary price: ${cake >> 5}");
-    Console.WriteLine($"Real price: ${(cake >> 5) * 50}\n\n");
+    Console.WriteLine($"Real price: ${(cake >> 5) * 50}\n");
 }
+Console.WriteLine("---------------- Bakery Fresh Cake ----------------");
+DisplayCake(nameof(famous_house_signatureredvelvet), famous_house_signatureredvelvet);
+DisplayCake(nameof(red_velvet_cheese_cake), red_velvet_cheese_cake);
+DisplayCake(nameof(black_forest_cake), black_forest_cake);
+DisplayCake(nameof(choco_mar_cake), choco_mar_cake);
+DisplayCake(nameof(strawberry_vanilla), strawberry_vanilla);
+DisplayCake(nameof(ginger_lemon), ginger_lemon);
+DisplayCake(nameof(coffee_nut), coffee_nut);
+DisplayCake(nameof(vanilla_choco), vanilla_choco);
+
+
+
+Console.WriteLine("--- Special Category Cake ---");
+Console.WriteLine("--- Cookies ---");
+
 DisplayCake(nameof(muffins), muffins);
 DisplayCake(nameof(chocco_muffins), chocco_muffins);
 DisplayCake(nameof(de_muffins_theme), de_muffins_theme);
 DisplayCake(nameof(eggless_cake), eggless_cake);
 DisplayCake(nameof(diabetie_cake), diabetie_cake);
-DisplayCake(nameof(black_forest_cake), black_forest_cake);
-DisplayCake(nameof(red_velvet_cheese_cake), red_velvet_cheese_cake);
 DisplayCake(nameof(caramel_cake), caramel_cake);
-DisplayCake(nameof(choco_mar_cake), choco_mar_cake);
 #endregion
 
 
@@ -57,6 +73,11 @@ uint orderLine6 = (uint)(black_forest_cake | (1 << 11));
 uint orderLine7 = (uint)(red_velvet_cheese_cake | (2 << 11));
 uint orderLine8 = (uint)(caramel_cake | (5 << 11));
 uint orderLine9 = (uint)(choco_mar_cake | (1 << 11));
+uint orderLine10 = (uint)(strawberry_vanilla | (2 << 11));
+uint orderLine11 = (uint)(ginger_lemon | (4 << 11));
+uint orderLine12 = (uint)(famous_house_signatureredvelvet | (1 << 11));
+uint orderLine13 = (uint)(coffee_nut | (2 << 11));
+uint orderLine14 = (uint)(vanilla_choco | (3 << 11));
 
 void DisplayOrder(string name, uint orderLine)
 {
@@ -80,6 +101,11 @@ DisplayOrder(nameof(black_forest_cake), orderLine6);
 DisplayOrder(nameof(red_velvet_cheese_cake), orderLine7);
 DisplayOrder(nameof(caramel_cake), orderLine8);
 DisplayOrder(nameof(choco_mar_cake), orderLine9);
+DisplayOrder(nameof(strawberry_vanilla), orderLine10);
+DisplayOrder(nameof(ginger_lemon), orderLine11);
+DisplayOrder(nameof(famous_house_signatureredvelvet), orderLine12);
+DisplayOrder(nameof(coffee_nut), orderLine13);
+DisplayOrder(nameof(vanilla_choco), orderLine14);
 #endregion
 
 
